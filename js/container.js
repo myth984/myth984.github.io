f34aa9c1c1e84867583ae7b40f821c1a5a065d4b
myth <119322118@qq.com>
2020-09-19 02:32:30 +0800

    Site updated: 2020-09-19 02:32:25

--- js/container.js
+++ js/container.js
@@ -1,7 +1,21 @@
 const loadPostInfo = function() {
-    document.querySelectorAll("#hotVideosSection .videoPreviewBg img").forEach(i => {
-        i.src = 'http://lorempixel.com/258/145/?v=' + Math.floor(Math.random() * 10000)
-    })
+    let imageSet = new Set();
+    let imageHtmllCollection = document.querySelectorAll("#hotVideosSection .videoPreviewBg img")
+    let maxImageSum = 316;
+    if(imageHtmllCollection.length < maxImageSum){
+        // 保证图片不重复
+        while (imageSet.size < maxImageSum){
+            imageSet.add(randNum(1,316))
+        }
+        let imageArray = Array.from(imageSet);
+        imageHtmllCollection.forEach((i,n) => {
+            i.src = '/image/blog-image/' + imageArray[n] + '.jpg'
+        })
+    }else{
+        imageHtmllCollection.forEach(i => {
+            i.src = '/image/blog-image/' + randNum(1,316) + '.jpg'
+        })
+    }
     for (let i of document.getElementsByClassName("js-viewCount")) {
         let r = Math.random()
         i.innerHTML = Math.round(r * 1000) + (r > 0.5 ? 'M' : 'K') + '次观看'
@@ -11,6 +25,9 @@ const loadPostInfo = function() {
         let min = 70;
         let max = 100;
         i.innerHTML = parseInt(Math.random() * (max - min + 1) + min, 10) + '%'
+    }
+    function randNum(min,max){
+        return Math.floor(Math.random() * (max - min + 1) + min);
     }
 }
 loadPostInfo()
