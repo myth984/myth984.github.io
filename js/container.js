9d998119d67272b906253568e6da5aeccee3c66e
myth <119322118@qq.com>
2020-09-26 13:31:31 +0800

    Site updated: 2020-09-26 13:31:29

--- js/container.js
+++ js/container.js
@@ -44,11 +44,3 @@ document.getElementById('closeSwitchEnglish').onclick = () => {
     document.getElementById('countryRedirectMessage').remove();
 }
 
-document.getElementsByClassName('phoneMenu')[0].onclick = () => {
-    if(document.getElementById('phoneMenuPanel').style.display=='block'){
-        document.getElementById('phoneMenuPanel').style.display='none'
-    }else{
-        document.getElementById('phoneMenuPanel').style.display='block'
-
-    }
-}
