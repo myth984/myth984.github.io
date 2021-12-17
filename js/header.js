3114cf2df993c6c1470cbf75542969f503e24188
yantianhao <mythjava@protonmail.com>
2021-12-17 11:29:32 +0800

    Site updated: 2021-12-17 11:29:20

--- js/header.js
+++
@@ -1,7 +0,0 @@
-document.getElementsByClassName('phoneMenu')[0].onclick = () => {
-    if(document.getElementById('phoneMenuPanel').style.display=='block'){
-        document.getElementById('phoneMenuPanel').style.display='none'
-    }else{
-        document.getElementById('phoneMenuPanel').style.display='block'
-    }
-}
